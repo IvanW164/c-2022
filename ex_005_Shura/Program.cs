f9dbfc1c48a463bfb786ad7ec6df5bf7e13eb1ff
IvanKratenko <kratenko_ivan@mail.ru>
2022-01-08 00:02:41 +0700

    Shura

---
+++ ex_005_Shura/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "шура")
+{
+    Console.WriteLine("Ура это Шура!");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
